d4929d94005adcb877279fbfd620d1730652e0e1
Rodion Moiseev <grachyovandryushka@yandex.ru>
2019-11-06 20:00:01 +0300

    Add lesson4_java files

---
+++ math/src/hello/hello.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class hello {
+	public static void main(String[] args) {
+		System.out.println("*    * ***** *     *     **** ");
+		System.out.println("*    * *     *     *    *    * ");
+		System.out.println("****** ***** *     *    *    * ");
+		System.out.println("*    * *     *     *    *    * ");
+		System.out.println("*    * ***** ***** ***** **** ");
+		
+	}
+}
